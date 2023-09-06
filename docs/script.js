f68d3b479da16984244e55cbd9befab469f0b4d6
Lesiba Moshweu <moshweutipe@gmail.com>
2023-09-06 20:26:45 +0200

    copleted desktop

--- docs/script.js
+++ docs/script.js
@@ -10,9 +10,14 @@ fetch(path)
     
     developer = data.developer
     physicaladdress = data.physicalAddress
+    menu = data.menu
+
+    div_starter = document.getElementById('starter')    
+    div_kota = document.getElementById('kota')
+    div_chips = document.getElementById('chips')
+    div_sides = document.getElementById('sides')
 
     const footer = document.getElementById('footer')
-    //const dev_content = document.getElementById('dev-content')
 
     div_addressElement = document.createElement('div'); 
     div_dev_contentElement = document.createElement('div'); 
@@ -22,15 +27,45 @@ fetch(path)
     //devElement = document.getElementById('dev-content') <p><a href="html_images.asp">HTML Images</a></p>
 
     //addressElement = '<address>' + physicaladdress.street + '<br>' + physicaladdress.city +'<br>'+ physicaladdress.province +'<br>'+ physicaladdress.code + '</address>'
-    addressElement = '<address id="address">' + physicaladdress.street + '<br>' + physicaladdress.city +'<br>'+ physicaladdress.province +'<br>'+ physicaladdress.code + '</address>'
+    //array1.forEach((element) => console.log(element));
+    console.log('Menu.STARTER: ',menu.starter)
+
+    p_starterElement = '<p>'
+    menu.starter.forEach(element => {p_starterElement += element + '<br>'});
+    p_starterElement += '</p>'
+    div_starter.innerHTML += p_starterElement
+    
+    p_kotaElement = '<p>'
+    menu.kota.forEach(element => {p_kotaElement += element + '<br>'});
+    p_kotaElement += '</p>'
+    div_kota.innerHTML += p_kotaElement
+
+    p_chipsElement = '<p>'
+    menu.chips.forEach(element => {p_chipsElement += element + '<br>'});
+    p_chipsElement += '</p>'
+    div_chips.innerHTML += p_chipsElement
+
+    p_sidesElement = '<p>'
+    menu.sides.forEach(element => {p_sidesElement += element + '<br>'});
+    p_sidesElement += '</p>'
+    div_sides.innerHTML += p_sidesElement
+
+
+    addressElement = '<address id="address"> <b>Physical Address</b> <br> ' + physicaladdress.street + '<br>' + physicaladdress.city +'<br>'+ physicaladdress.province +'<br>'+ physicaladdress.code + '</address>'
     dev_contentElement = '<p id="dev-content"> Developed by: <a style="color:#ffffff" href="https://www.tipegraphics.co.za/"> ' + developer.name + '</a>   | '+ new Date().getFullYear() + ' | &copy; Copyright reserved<p/>'
 
     //div_addressElement.append(addressElement)
+    //div_starter.innerHTML += p_starterElement
+
+
+
     div_addressElement.innerHTML += addressElement
     div_dev_contentElement.innerHTML += dev_contentElement
 
     footer.append(div_addressElement)
+    //footer.append(div_)
     footer.append(div_dev_contentElement)
+
 
   })
   .catch(error => {
